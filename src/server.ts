43c51f791d07e1215cb0ca934b395c7bd76889d3
João Mangueira <jamjs7@gmail.com>
2023-01-20 11:03:42 -0300

    ajustando erros

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-console.log("oi")
+import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const app = Fastify();
+const prisma = new PrismaClient();
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'Beber'
+      }
+    }
+  });
+
+  return habits;
+});
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server running!');
+});
