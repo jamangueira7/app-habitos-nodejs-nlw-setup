3a9bb87b8c483872061c94bf3f5b363069b0f7f6
João Mangueira <jamjs7@gmail.com>
2023-01-20 11:06:38 -0300

    add o cors

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
 const prisma = new PrismaClient();
+
+app.register(cors);
 app.get('/', async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Beber'
-      }
-    }
-  });
+  const habits = await prisma.habit.findMany();
 
   return habits;
 });
