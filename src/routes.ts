0f317399728a1e995292e19a95b2b654944992fd
João Mangueira <jamjs7@gmail.com>
2023-01-23 10:45:05 -0300

    ajustando detalhes da rota

--- src/routes.ts
+++ src/routes.ts
@@ -36,7 +36,9 @@ export async function appRoutes(app: FastifyInstance) {
 
     const { date } = createHabitBody.parse(request.query);
 
-    const weekDay = dayjs(date).get('day');
+    const parsedDate = dayjs(date).startOf('day');
+
+    const weekDay = parsedDate.get('day');
 
     const possibleHabits = await prisma.habit.findMany({
       where: {
@@ -51,8 +53,48 @@ export async function appRoutes(app: FastifyInstance) {
       }
     });
 
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      }
+    });
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id;
+    });
+
     return {
       possibleHabits,
+      completedHabits,
     };
+  });
+
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6))
+    });
+
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    const today = dayjs().startOf('day').toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create:weekDays.map(weekDay => {
+            return {
+              week_day: weekDay,
+            }
+          })
+        }
+      }
+    });
+
   });
 }
